735f2f7ef787f47327f84851b5faaf9b9105480b
tchami <131756888+tchamianest@users.noreply.github.com>
2023-10-16 13:46:10 +0200

    continue with number and dates

--- 11-starter folder/script.js
+++ 11-starter folder/script.js
@@ -551,3 +551,53 @@ const z = Array.from({ length: 100 }, (cue, i) =>
 );
 console.log(z);
 ////💥💥💥💥💥💥💥💥End ofnew way to create array💥💥💥💥💥💥
+
+//final challenge
+// // 1.
+// dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+
+// // 2.
+// const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+// console.log(dogSarah);
+// console.log(
+//   `Sarah's dog is eating too ${
+//     dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+//   } `
+// );
+
+// // 3.
+// const ownersEatTooMuch = dogs
+//   .filter(dog => dog.curFood > dog.recFood)
+//   .flatMap(dog => dog.owners);
+// // .flat();
+// console.log(ownersEatTooMuch);
+
+// const ownersEatTooLittle = dogs
+//   .filter(dog => dog.curFood < dog.recFood)
+//   .flatMap(dog => dog.owners);
+// console.log(ownersEatTooLittle);
+
+// // 4.
+// // "Matilda and Alice and Bob's dogs eat too much!"
+// //  "Sarah and John and Michael's dogs eat too little!"
+// console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+// console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+// // 5.
+// console.log(dogs.some(dog => dog.curFood === dog.recFood));
+
+// // 6.
+// // current > (recommended * 0.90) && current < (recommended * 1.10)
+// const checkEatingOkay = dog =>
+//   dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+
+// console.log(dogs.some(checkEatingOkay));
+
+// // 7.
+// console.log(dogs.filter(checkEatingOkay));
+
+// // 8.
+// // sort it by recommended food portion in an ascending order [1,2,3]
+// const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+// console.log(dogsSorted);
+// */
