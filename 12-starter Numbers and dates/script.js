c835efb224f57583bbc18a83f07f0fcf06a3ebcc
tchami <131756888+tchamianest@users.noreply.github.com>
2023-10-20 12:58:05 +0200

    free

--- 12-starter Numbers and dates/script.js
+++ 12-starter Numbers and dates/script.js
@@ -460,18 +460,18 @@ updateTime();
 const intervalId = setInterval(updateTime, 1000); //for update our function every second
 // //////////////////////⌚⌚⌚⌚⌚⌚⌚⌚⌚real time
 
-let time = 110;
+// let time = 110;
 
-const func = function () {
-  const dispaly = function () {
-    const minuter = Math.trunc(time / 60);
-    const second = Math.trunc(time % 60);
+// const func = function () {
+//   const dispaly = function () {
+//     const minuter = Math.trunc(time / 60);
+//     const second = Math.trunc(time % 60);
 
-    time--;
-    document.body.innerHTML = `${minuter}:${second}`;
-  };
-  dispaly();
-  const kalisa = setInterval(dispaly, 1000);
-};
+//     time--;
+//     document.body.innerHTML = `${minuter}:${second}`;
+//   };
+//   dispaly();
+//   const kalisa = setInterval(dispaly, 1000);
+// };
 
-func();
+// func();
