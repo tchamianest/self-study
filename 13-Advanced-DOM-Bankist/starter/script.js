d3ae532017287e197a63824000c1b5a694fefbdd
tchami <131756888+tchamianest@users.noreply.github.com>
2023-10-25 12:10:12 +0200

    stick header
    i learn how to stick header based on position it reach when you scroll

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -9,7 +9,10 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScroll = document.querySelector('.btn--scroll-to');
 const section1 = document.getElementById('section--1');
-
+const tabs = document.querySelectorAll('.operations__tab');
+const tabscontainer = document.querySelector('.operations__tab-container');
+const contenr = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -33,11 +36,49 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️Implement stick navigation↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️
+//stick navigation
+const boundary = section1.getBoundingClientRect();
+window.addEventListener('scroll', function () {
+  if (window.scrollY > boundary.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+///↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️End of Implement stick navigation↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️Menu fade animation↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️
+const handelehover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this; //opacity;
+    });
+    logo.style.opacity = this; //opacity;
+  }
+};
+/// first way to handle that to call event in the functon before
+// nav.addEventListener('mouseover', function (e) {
+//   handelehover(e, 0.5);
+// });
+// nav.addEventListener('mouseout', function (e) {
+//   handelehover(e, 1);
+// });
+
+//bind methode for use function insider other
+//parsing argument into handle it difficulrt way
+nav.addEventListener('mouseover', handelehover.bind(0.5));
+nav.addEventListener('mouseout', handelehover.bind(1));
+///↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️End of Menu fade animation↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 
 ///↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️Tabbed component↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️↗️
-const tabs = document.querySelectorAll('.operations__tab');
-const tabscontainer = document.querySelector('.operations__tab-container');
-const contenr = document.querySelectorAll('.operations__content');
 
 //this is to select each tab that we click
 // tabs.forEach(tab =>
