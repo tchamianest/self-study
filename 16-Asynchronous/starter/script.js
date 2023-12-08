e56e7d817282819f6583e7767ae405e6c17cdef4
tchami <131756888+tchamianest@users.noreply.github.com>
2023-12-08 10:52:33 +0200

    build also Neighbors'
    render also second countries and refactor my code with building methods outside for rendering.

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -7,19 +7,44 @@ const countriesContainer = document.querySelector('.countries');
 
 ///way for get request
 
-const getcountry = function (countrie) {
-  const request = new XMLHttpRequest();
-  request.open('Get', `https://restcountries.com/v3.1/name/${countrie}`);
+// const getcountry = function (countrie) {
+//   const request = new XMLHttpRequest();
+//   request.open('Get', `https://restcountries.com/v3.1/name/${countrie}`);
 
-  //we send  to the server
-  request.send();
+//   //we send  to the server
+//   request.send();
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    const html = `
-        <article class="country">
+//     const html = `
+//         <article class="country">
+//           <img class="country__img" src="${data.flags.svg}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name.common}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               +data.population / 1000000
+//             ).toFixed(1)}M Population</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
+//             <p class="country__row"><span>💰</span>${data.fifa}</p>
+//           </div>
+//         </article>
+//   `;
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+// getcountry('rwanda');
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////// GET COUNTRY WITH NEIGHBOR
+
+/// make function for rendering
+const countryRender = function (data, classadd = '') {
+  const html = `
+        <article class="country ${classadd}">
           <img class="country__img" src="${data.flags.svg}" />
           <div class="country__data">
             <h3 class="country__name">${data.name.common}</h3>
@@ -32,11 +57,39 @@ const getcountry = function (countrie) {
           </div>
         </article>
   `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getcountryNeigbour = function (countrie) {
+  const request1 = new XMLHttpRequest();
+  request1.open('Get', `https://restcountries.com/v3.1/name/${countrie}`);
+
+  //we send  to the server
+  request1.send();
+
+  request1.addEventListener('load', function () {
+    const [data1] = JSON.parse(this.responseText);
+    console.log(data1);
+
+    /// Render country one
+    countryRender(data1);
+
+    /// GET NEIGHBOUR COUNTRY
+    const [neighbour] = data1.borders;
+    console.log(neighbour);
+
+    const request2 = new XMLHttpRequest();
+    request2.open('Get', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      ///RENDER COUNTRY 2
+      countryRender(data2, 'neighbour');
+    });
   });
 };
-getcountry('rwanda');
-getcountry('usa');
-getcountry('canada');
-getcountry('germany');
+getcountryNeigbour('usa');
